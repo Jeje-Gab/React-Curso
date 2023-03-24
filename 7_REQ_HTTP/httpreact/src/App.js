8bcb2132f1193a2840bc65814e5efb3e4f123fe7
gabriel.jesus <gabrielsj151@gmail.com>
2023-03-24 16:35:06 -0300

    adicionado metodo POST para envio das informações ao BackEnd

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -12,6 +12,9 @@ function App() {
   // "products" -> Auxilia a salvar os pordutos
   // "setProducts" -> Auxilia a colocar os produtos em algum lugar...
 
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
   // 1 - REsgatando dados
   useEffect(() => {
     async function fetchData() {
@@ -30,7 +33,42 @@ function App() {
   /*array de dependencia*/
 
   console.log(products);
-  
+
+  // 2 - Add de produtos
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // preventDefault -> Nâo submete o formulario
+
+    // Objeto criado para enviar todos os dados dele para o Backend
+    const product = {
+      name,
+      price,
+    };
+    console.log(product);
+    
+    // requisição } GET
+    const res = await fetch(url, {
+      // Configura como vai ser a resquisição
+      method: "POST",
+      // Cabeçalhos da Requisição // Transmitir na requisição que tipo de conteudo está transmitindo/Passando
+      headers: {
+        "Content-Type": "application/json"
+      }, 
+      // Corpo da requisição --> é o Objeto "Product"
+      body: JSON.stringify(product),
+    });
+    // 3 - Carregamento Dinamico
+    const addedProduct = await res.json(); // Transforma um Json em Um objeto
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]) // Addiciona os antigos produtos e o novo evitando ter que carregar a pagina para atualizar os mesmos
+    // prevProducts -> Produtos antes de add um novo
+    // addedProduct -> Novo produto que veio de um JSON
+
+    // Resetar os States
+    setName(""); // Ao manipular um state -> Manipula-se os estados dos inputs
+    setPrice("");
+  };
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
@@ -41,6 +79,26 @@ function App() {
           </li>
         ))}
       </ul>
+      {/*Formulario*/}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome: 
+            <input type="text" 
+            value={name} 
+            name="name" 
+            onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço: 
+            <input type="number" 
+            value={price} 
+            name="price" 
+            onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value="Criar"/>
+        </form>
+      </div>
     </div>
   );
 }
